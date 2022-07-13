1a47af001af936e46e1315e36e3c5becbdf76dd3
pradeep-ravan <triplehthegame143@gmail.com>
2022-07-13 16:12:07 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+// https://62cea09f132eeb0060b1ed25--dynamic-sprite-5393af.netlify.app/  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and savdsfe to Reload.
+          Edit <code>src/App.js</code> and savdsfe to Reloaddfd.
+        </p>
+        <p>
+          dsdsdsds
         </p>
         <a
           className="App-link"
